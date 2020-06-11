60d3bdf5ec925c8dc237c0dd156d4a80095bc010
e212950 <anup.shanbhag@evry.com>
2020-06-11 10:41:20 +0200

    Added Formatting and Indentation

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#page-hdr").load("../index.html #page-hdr > *");
 });
--- scripts/post.js
+++ scripts/post.js
@@ -1,56 +1,56 @@
-var likesCount=0;
-var commentsCount=0;
+var likesCount = 0;
+var commentsCount = 0;
 document.getElementById('owner').innerText = sessionStorage.getItem('owner');
 document.getElementById('title').innerText = sessionStorage.getItem('title');
 document.getElementById('content').innerText = sessionStorage.getItem('content');
 updateLikesOnPage();
 
-function updateLikesOnPage(){
-    if(likesCount === 0){
+function updateLikesOnPage() {
+    if (likesCount === 0) {
         document.getElementById('likes').innerText = 'Be the first one to like this!'
     }
-    else{
+    else {
         document.getElementById('likes').innerText = likesCount + ' people like this!'
     }
-    
+
 }
 
-function likePost(){
+function likePost() {
     likesCount++;
-    document.getElementById('btn-like').innerHTML='<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked</i>';
+    document.getElementById('btn-like').innerHTML = '<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked</i>';
     updateLikesOnPage();
 }
 
-function addComment(text){
-    if(text.value.trim() !== 0){
+function addComment(text) {
+    if (text.value.trim() !== 0) {
         commentsCount++;
-        if(commentsCount === 0){
+        if (commentsCount === 0) {
             document.getElementById('comments').style.display = 'none';
         }
-        else{
+        else {
             document.getElementById('comments').style.display = 'block';
         }
         var comment = '<p class="comment text-justify" id="' + commentsCount + '">' + text.value + '</p>';
         document.getElementById('comments').innerHTML = comment + document.getElementById('comments').innerHTML;
     }
-    text.value='';
+    text.value = '';
 }
-function editPost(){
-    document.getElementById('btn-edit').style.display='none';
-    document.getElementById('btn-save').style.display='block';
+function editPost() {
+    document.getElementById('btn-edit').style.display = 'none';
+    document.getElementById('btn-save').style.display = 'block';
     var title = document.getElementById('title'), content = document.getElementById('content');
-    title.contentEditable=true;
-    content.contentEditable=true;
+    title.contentEditable = true;
+    content.contentEditable = true;
     title.classList.add('editable');
     content.classList.add('editable');
 }
 
-function savePost(){
-    document.getElementById('btn-edit').style.display='block';
-    document.getElementById('btn-save').style.display='none';
+function savePost() {
+    document.getElementById('btn-edit').style.display = 'block';
+    document.getElementById('btn-save').style.display = 'none';
     var title = document.getElementById('title'), content = document.getElementById('content');
-    title.contentEditable=true;
-    content.contentEditable=true;
+    title.contentEditable = true;
+    content.contentEditable = true;
     title.classList.remove('editable');
     content.classList.remove('editable');
 }
