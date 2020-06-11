751c9ea63eaf7fd6ed0363cbf20b1a9e54e18cd8
e212950 <anup.shanbhag@evry.com>
2020-06-11 02:19:46 +0200

    HTML and Styling tweaks and added java script event handlers

---
+++ scripts/post.js
@@ -0,0 +1,46 @@
+var likesCount=0;
+var commentsCount=0;
+document.getElementById('owner').innerText = sessionStorage.getItem('owner');
+document.getElementById('title').innerText = sessionStorage.getItem('title');
+document.getElementById('content').innerText = sessionStorage.getItem('content');
+updateLikesOnPage();
+
+function updateLikesOnPage(){
+    if(likesCount === 0){
+        document.getElementById('likes').innerText = 'Be the first one to like this!'
+    }
+    else{
+        document.getElementById('likes').innerText = likesCount + ' people like this!'
+    }
+    
+}
+
+function likePost(){
+    likesCount++;
+    document.getElementById('btn-like').innerHTML='<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked</i>';
+    updateLikesOnPage();
+}
+
+function addComment(text){
+    if(text.value.trim() !== 0){
+        commentsCount++;
+        if(commentsCount === 0){
+            document.getElementById('comments').style.display = 'none';
+        }
+        else{
+            document.getElementById('comments').style.display = 'block';
+        }
+        var comment = '<p class="comment text-justify" id="' + commentsCount + '">' + text.value + '</p>';
+        document.getElementById('comments').innerHTML = comment + document.getElementById('comments').innerHTML;
+    }
+    text.value='';
+}
+function editPost(){
+    document.getElementById('btn-edit').style.display='none';
+    document.getElementById('btn-save').style.display='block';
+}
+
+function savePost(){
+    document.getElementById('btn-edit').style.display='block';
+    document.getElementById('btn-save').style.display='none';
+}
--- scripts/postslist.js
+++ scripts/postslist.js
@@ -11,6 +11,6 @@ function deletePost() {
 function getPost(id) {
     sessionStorage.setItem("owner",document.getElementById(id).querySelector('.post-owner > p').innerText);
     sessionStorage.setItem('title',document.getElementById(id).querySelector('.title-text').innerText);
-    sessionStorage.setItem('text',document.getElementById(id).querySelector('.card-text').innerText);
+    sessionStorage.setItem('content',document.getElementById(id).querySelector('.card-text').innerText);
     window.location.href = './post.html';
 }
